654f1349d455c620add1f9665022c7328ce3be6d
vinayak <vdy1141@gmail.com>
2025-01-13 18:56:20 +0530

    Added functionality to calculate factorial of a number

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Hello World")
-print("How are you?")
+def factorial(num):
+    if num < 0:
+        print("Can't calculate factorial")
+        return
+    if num <= 1:
+        print("Factorial is ", 1)
+        return
+    fact = 1
+    for i in range(2, num+1):
+        fact = fact * i
+    print("The factorial is", fact)
+    return
+
+factorial(5)
